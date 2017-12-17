adbbcbb7e55957849c637e4aecfab8db8204534a
Stanisław Malc <dorianus@g.pl>
2017-12-17 15:07:20 +0100

    Initial commit

---
+++ Assets/Scripts/LooseCollider.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LooseCollider : MonoBehaviour {
+
+	void OnTriggerEnter2D (Collider2D trigger) {
+		print("trigger");
+	}
+
+	void OnCollisionEnter2D (Collision2D collision) {
+		print("Collision");
+	}
+
+
+}
