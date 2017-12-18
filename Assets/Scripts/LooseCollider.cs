fbc84cf0f7c6625c23254651f8234d9b54843f3e
Stanisław Malc <dorianus@g.pl>
2017-12-18 22:16:23 +0100

    Update 2
    Add Collider Elements and mouse movemenet block

--- Assets/Scripts/LooseCollider.cs
+++ Assets/Scripts/LooseCollider.cs
@@ -4,8 +4,13 @@ using UnityEngine;
 
 public class LooseCollider : MonoBehaviour {
 
+	public LevelManager levelManager;
+
 	void OnTriggerEnter2D (Collider2D trigger) {
 		print("trigger");
+		levelManager.LoadLevel("Win");
+
+
 	}
 
 	void OnCollisionEnter2D (Collision2D collision) {
